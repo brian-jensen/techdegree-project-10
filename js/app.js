a0f637cb8473242d34ee3b2ec60c9cfa4cae6d39
Brian Jensen <31598338+brian-jensen@users.noreply.github.com>
2018-04-06 10:07:30 -0700

    finished core layout styles for all screen sizes

--- js/app.js
+++ js/app.js
@@ -1,38 +1,44 @@
 /* jshint esversion: 6 */
 
+const directoryContainer = document.querySelector('main');
+let employees = [];
+
 $.ajax({
   url: 'https://randomuser.me/api/?results=12&nat=us',
   dataType: 'json',
   success: function(data) {
+    employees = data.results;
     buildDirectory(data);
   }
 });
 
 function buildDirectory(data) {
-  const directoryContainer = document.querySelector('main');
-
-  for (const key in data.results) {
-    let image = data.results[key].picture.large;
-    let firstName = data.results[key].name.first;
-    let lastName = data.results[key].name.last;
+  for (const key in employees) {
+    let image = employees[key].picture.large;
+    let firstName = employees[key].name.first;
+    let lastName = employees[key].name.last;
     let fullName = capitalize(`${firstName} ${lastName}`);
-    let email = data.results[key].email;
-    let cell = data.results[key].phone;
-    let street = data.results[key].location.street;
-    let city = data.results[key].location.city;
-    let state = data.results[key].location.state;
-    let zipcode = data.results[key].location.postcode;
-    let dob = new Date(Date.parse(data.results[key].dob.replace(/-/g, "/"))).toLocaleDateString(navigator.language);
+    let email = employees[key].email;
+    let cell = employees[key].phone;
+    let street = employees[key].location.street;
+    let city = employees[key].location.city;
+    let state = employees[key].location.state;
+    let zipcode = employees[key].location.postcode;
+    let dob = new Date(Date.parse(employees[key].dob.replace(/-/g, "/"))).toLocaleDateString(navigator.language);
 
     directoryContainer.innerHTML += `
       <div class="grid__col--4 employee">
         <ul class="employee-info">
-          <li><img class="employee-img" src="${image}" alt="Image of ${fullName}"></li>
+          <li class="grid__col--1 employee-image"><img class="employee-img" src="${image}" alt="Image of ${fullName}"></li>
+          <li class="grid__col--11">
+            <p class="employee-name">${fullName}</p>
+            <p class="employee-email">${email}</p>
+            <p class="employee-city">${city}</p>
+          </li>
         </ul>
       </div>
     `;
   }
-
 }
 
 function capitalize(name) {
@@ -40,3 +46,4 @@ function capitalize(name) {
     return firstLetter.toUpperCase();
   });
 }
+
